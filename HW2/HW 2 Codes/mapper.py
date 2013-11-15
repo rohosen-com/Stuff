ad1af517879b5800d1f3c70b02f22dfd9119d97b
rohosen-com <rohosenb@Rohosens-MacBook-Air.local>
2013-11-14 16:55:08 -0800

    Codes of HW2
    Codes uploaded

--- HW2/HW 2 Codes/mapper.py
+++ HW2/HW 2 Codes/mapper.py
@@ -10,6 +10,9 @@ for line in sys.stdin:
     # split the line into words
     numbers = line.split('\t')
 
+    # Calculate the key
+    # For each pair of observation it produces x_lo and y_lo as the key
+    # x_lo and y_lo of the bin in which the observation should be
     n = float((math.floor(10*float(numbers[0])))/10), float((math.floor(10*float(numbers[1])))/10)
     num_st1 = str(n[0])
     num_st2 = str(n[1])
